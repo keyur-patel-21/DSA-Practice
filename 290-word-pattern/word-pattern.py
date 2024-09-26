c376d063472f308e5dff741782a28aa3b343c9bb
keyur-patel-21 <100202156+keyur-patel-21@users.noreply.github.com>
2024-09-26 05:39:47 -0400

    Time: 18 ms (17.88%) | Memory: 11.8 MB (9.53%) - LeetSync

---
+++ 290-word-pattern/word-pattern.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def wordPattern(self, pattern, s):
+        """
+        :type pattern: str
+        :type s: str
+        :rtype: bool
+        """
+        s = s.split(" ")
+        mapPS, mapSP = {}, {}
+
+        for i in range(len(pattern)):
+
+            c1, c2 = pattern[i], s[i]
+
+            if((c1 in mapPS and mapPS[c1] != c2) or (c2 in mapSP and mapSP[c2] != c1) or len(pattern) != len(s)):
+                return False
+
+            mapPS[c1] = c2
+            mapSP[c2] = c1
+        
+        return True
