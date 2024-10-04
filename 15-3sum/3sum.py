45d59edea3837644cc2223093b55b07188b3bb2e
keyur-patel-21 <100202156+keyur-patel-21@users.noreply.github.com>
2024-10-04 06:43:55 -0400

    Time: 610 ms (96.43%) | Memory: 15.4 MB (29.88%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -6,7 +6,8 @@ class Solution(object):
         for i, n in enumerate(nums):
             if i > 0 and n == nums[i - 1]:
                 continue
-
+            if nums[i] > 0:
+                break
             l, r = i + 1, len(nums) - 1
             while l < r:
                 answer = n + nums[l] + nums[r]
