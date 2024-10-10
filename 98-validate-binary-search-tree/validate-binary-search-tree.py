5180169a5debb634d3d030655097aa926b08e2ce
keyur-patel-21 <100202156+keyur-patel-21@users.noreply.github.com>
2024-10-09 20:41:13 -0400

    Time: 34 ms (11.36%) | Memory: 16.1 MB (77.00%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -9,11 +9,8 @@ class Solution(object):
         def valid(root, left, right):
             if not root:
                 return True
-            if not (root.val < right and root.val > left):
+            if not (root.val > left and root.val < right):
                 return False
-
             return (valid(root.left, left, root.val) and valid(root.right, root.val, right))
-
-        return valid(root, float("-inf"), float("inf"))
-
         
+        return(valid(root, float("-inf"), float("inf")))
