493dfca6ff3dc15853ed08c4dbb578bdf41f0afb
keyur-patel-21 <100202156+keyur-patel-21@users.noreply.github.com>
2024-10-14 05:32:49 -0400

    Time: 30 ms (98.31%) | Memory: 17.3 MB (86.72%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -1,14 +1,28 @@
-
 class Solution(object):
     def buildTree(self, preorder, inorder):
-        midIdx = {}
-        for i, n in enumerate(inorder):
-            midIdx[n] = i 
-        if not (preorder or inorder):
-            return None
-        root = TreeNode(preorder[0])
-        mid = midIdx[preorder[0]]
-        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
-        root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
-        return root
+        # Hashmap to store the index of values in the inorder traversal
+        midIdx = {n: i for i, n in enumerate(inorder)}
         
+        # Helper function to build the tree using preorder and inorder indices
+        def helper(pre_left, pre_right, in_left, in_right):
+            if pre_left > pre_right:
+                return None
+
+            # The first element in preorder is always the root
+            root_val = preorder[pre_left]
+            root = TreeNode(root_val)
+
+            # Get the index of the root in inorder traversal
+            mid = midIdx[root_val]
+            
+            # Number of nodes in the left subtree
+            left_subtree_size = mid - in_left
+
+            # Recursively build the left and right subtrees
+            root.left = helper(pre_left + 1, pre_left + left_subtree_size, in_left, mid - 1)
+            root.right = helper(pre_left + left_subtree_size + 1, pre_right, mid + 1, in_right)
+            
+            return root
+        
+        # Call the helper with initial indices for the full range
+        return helper(0, len(preorder) - 1, 0, len(inorder) - 1)
