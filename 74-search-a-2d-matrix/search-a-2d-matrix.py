a66667c896b4bda35b0d02406378e66f9ca0f51a
keyur-patel-21 <100202156+keyur-patel-21@users.noreply.github.com>
2025-02-09 15:31:23 -0500

    Time: 0 ms (100.00%) | Memory: 12.7 MB (23.21%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,40 +1,31 @@
 class Solution(object):
     def searchMatrix(self, matrix, target):
-        """
-        :type matrix: List[List[int]]
-        :type target: int
-        :rtype: bool
-        """
-        no_rows, no_columns = len(matrix), len(matrix[0])
+        no_of_rows, no_of_cols = len(matrix), len(matrix[0])
 
-        low_row, high_row = 0, no_rows - 1
+        top, bottom = 0, no_of_rows - 1
 
-        while(low_row <= high_row):
-            mid_row = (low_row + high_row) // 2
-
+        while top <= bottom:
+            mid_row = (top+bottom) // 2
             if target < matrix[mid_row][0]:
-                high_row = mid_row - 1
+                bottom = mid_row - 1
             elif target > matrix[mid_row][-1]:
-                low_row = mid_row + 1
+                top = mid_row + 1
             else:
                 break
 
-        if not (low_row <= high_row):
+        if not (top <= bottom):
             return False
 
-        main_row = (low_row + high_row) // 2
-        low, high = 0, no_columns - 1
+        row = (top + bottom) // 2
+        l, r = 0, no_of_cols - 1
 
-        while(low<=high):
-            mid = (low + high) // 2
-
-            if target < matrix[main_row][mid]:
-                high = mid - 1
-            elif target > matrix[main_row][mid]:
-                low = mid + 1
-            else:
+        while l <= r:
+            mid = (l + r) // 2
+            if matrix[row][mid] == target:
                 return True
-        return False
-
-
+            elif target <= matrix[row][mid]:
+                r = mid - 1
+            else:
+                l = mid + 1
+        return False    
         
