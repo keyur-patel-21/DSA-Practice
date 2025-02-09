09911c3aff12acde89c72a2b1f1f6f5c2847d1a0
keyur-patel-21 <100202156+keyur-patel-21@users.noreply.github.com>
2025-02-09 15:39:12 -0500

    Time: 0 ms (100.00%) | Memory: 12.6 MB (83.68%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -6,26 +6,28 @@ class Solution(object):
 
         while top <= bottom:
             mid_row = (top+bottom) // 2
+
             if target < matrix[mid_row][0]:
                 bottom = mid_row - 1
             elif target > matrix[mid_row][-1]:
                 top = mid_row + 1
             else:
                 break
-
+        
         if not (top <= bottom):
             return False
 
-        row = (top + bottom) // 2
+        row = (top+bottom) // 2
         l, r = 0, no_of_cols - 1
 
         while l <= r:
             mid = (l + r) // 2
-            if matrix[row][mid] == target:
+
+            if target == matrix[row][mid]:
                 return True
             elif target <= matrix[row][mid]:
                 r = mid - 1
             else:
-                l = mid + 1
-        return False    
+                l = mid + 1 
+        return False
         
