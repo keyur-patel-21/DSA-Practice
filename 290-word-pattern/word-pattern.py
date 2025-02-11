cf58d8513f249e2d6735ede6145003c47da327be
keyur-patel-21 <100202156+keyur-patel-21@users.noreply.github.com>
2025-02-11 06:40:51 -0500

    Time: 0 ms (100.00%) | Memory: 12.4 MB (50.46%) - LeetSync

--- 290-word-pattern/word-pattern.py
+++ 290-word-pattern/word-pattern.py
@@ -1,23 +1,18 @@
 class Solution(object):
     def wordPattern(self, pattern, s):
-        """
-        :type pattern: str
-        :type s: str
-        :rtype: bool
-        """
-        s = s.split(" ")
+        
+        s = s.split()
+        if len(s) != len(pattern):
+            return False
         mapPS, mapSP = {}, {}
 
         for i in range(len(pattern)):
+            c1, s1 = pattern[i], s[i]
 
-            c1, c2 = pattern[i], s[i]
-
-            if((c1 in mapPS and mapPS[c1] != c2) or 
-                (c2 in mapSP and mapSP[c2] != c1) or
-                len(pattern) != len(s)):
+            if((c1 in mapPS and mapPS[c1] != s1) or (s1 in mapSP and mapSP[s1] != c1)):
                 return False
 
-            mapPS[c1] = c2
-            mapSP[c2] = c1
-        
-        return True
+            mapPS[c1] = s1
+            mapSP[s1] = c1
+
+        return True        
