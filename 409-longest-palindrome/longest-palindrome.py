1f84698e843bcd1a15b9becbd731a50930d4fd98
keyur-patel-21 <100202156+keyur-patel-21@users.noreply.github.com>
2025-02-14 09:29:54 -0500

    Time: 3 ms (73.85%) | Memory: 12.5 MB (52.44%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.py
+++ 409-longest-palindrome/longest-palindrome.py
@@ -1,16 +1,18 @@
 class Solution(object):
     def longestPalindrome(self, s):
+        
         seen = set()
-        res = 0
+        result = 0
+
         for c in s:
             if c in seen:
                 seen.remove(c)
-                res += 2
+                result += 2
             else:
                 seen.add(c)
 
-        if seen:
-            res += 1
-
-        return res
-        
+        if not seen:
+            return result
+        else:
+            result += 1
+            return result
