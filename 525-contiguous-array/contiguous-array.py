6e22e92266b9e3812107249d0336d06beb11c51e
keyur-patel-21 <100202156+keyur-patel-21@users.noreply.github.com>
2025-02-14 20:51:40 -0500

    Time: 103 ms (73.97%) | Memory: 16.7 MB (44.46%) - LeetSync

--- 525-contiguous-array/contiguous-array.py
+++ 525-contiguous-array/contiguous-array.py
@@ -1,16 +1,16 @@
 class Solution(object):
     def findMaxLength(self, nums):
-        
+
         if ((not nums) or (len(nums) == 0) or (len(nums) == 1)):
             return 0
         
         myDict = {}
 
-        rSum = 0
         res = 0
+        rSum = 0
 
         myDict[0] = -1
-
+        
         for i in range(len(nums)):
 
             if nums[i] == 0:
@@ -19,8 +19,9 @@ class Solution(object):
                 rSum += 1
 
             if rSum in myDict:
-                res = max(res, (i - myDict[rSum]))
+                res = max(res, (i - myDict[rSum])) 
             else:
-                myDict[rSum] = i
-        
-        return res
+                myDict[rSum] = i    
+
+
+        return res   
