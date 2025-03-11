f52a8443d3faf6cc3ce01fdd073f952a073543c0
keyur-patel-21 <100202156+keyur-patel-21@users.noreply.github.com>
2025-03-11 09:49:37 -0400

    Time: 0 ms (100.00%) | Memory: 12.5 MB (14.10%) - LeetSync

--- 289-game-of-life/game-of-life.py
+++ 289-game-of-life/game-of-life.py
@@ -3,30 +3,30 @@ class Solution(object):
         
         m = len(board)
         n = len(board[0])
-        
-        dirs = [[0,1] ,[1,0], [0,-1], [-1,0], [1,1], [-1,-1], [-1,1], [1,-1]]
 
         for i in range(m):
             for j in range(n):
+
+                dirs = [[0,1], [1,0], [0,-1], [-1,0], [1,1], [-1,-1], [-1,1], [1,-1]]
 
                 count = self.countAlive(i, j, board, dirs, m, n)
 
-                if(board[i][j] == 1):
-                    if(count < 2 or count > 3):
-                        board[i][j] = 2
-                elif(board[i][j] == 0):
-                    if(count == 3):
-                        board[i][j] = 3
+                if board[i][j] == 1:
+                    if (count < 2 or count > 3):
+                        board[i][j] = 2    #alive to dead
+                elif board[i][j] == 0:
+                    if count == 3:
+                        board[i][j] = 3    #dead to alive
 
-                        
+            
         for i in range(m):
             for j in range(n):
+
                 if board[i][j] == 2:
-                    board[i][j] = 0 
+                    board[i][j] = 0
                 elif board[i][j] == 3:
                     board[i][j] = 1
-
-
+        
     def countAlive(self, i, j, board, dirs, m, n):
 
         count = 0
@@ -35,10 +35,9 @@ class Solution(object):
             r = i + dir[0]
             c = j + dir[1]
 
-            if 0 <= r < m and 0 <= c < n:
+            if (0 <= r < m and 0 <= c < n):
                 if (board[r][c] == 1 or board[r][c] == 2):
                     count += 1
-
+        
         return count
-
 
