07719598d15c90c8d56663ce6fd5ccea8a18f9a5
keyur-patel-21 <100202156+keyur-patel-21@users.noreply.github.com>
2025-03-13 11:46:25 -0400

    Time: 0 ms (100.00%) | Memory: 12.3 MB (84.67%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -4,23 +4,24 @@ class Solution(object):
         p2 = n -1
         idx = (m+n) - 1
 
-        while p1 >= 0 and p2 >= 0:
-            if nums2[p2] > nums1[p1]:
-                nums1[idx] = nums2[p2]
-                p2 -= 1
-                idx -= 1
-            elif nums2[p2] < nums1[p1]:
+        while p1>=0 and p2 >=0:
+            if nums1[p1] > nums2[p2]:
                 nums1[idx] = nums1[p1]
-                p1 -= 1
                 idx -= 1
+                p1 -= 1
+            elif  nums2[p2] > nums1[p1]:
+                nums1[idx] = nums2[p2]
+                idx -= 1
+                p2 -= 1
             else:
                 nums1[idx] = nums1[p1]
-                p1 -= 1
                 idx -= 1
-        
+                p1 -= 1
+            
         while p2 >= 0:
             nums1[idx] = nums2[p2]
-            p2 -= 1
             idx -= 1
+            p2 -= 1
 
         return nums1
+        
