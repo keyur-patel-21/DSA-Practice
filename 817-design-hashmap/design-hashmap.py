f46b9fb04d98f3fd95479ce3da45d3a04ff7740d
keyur-patel-21 <100202156+keyur-patel-21@users.noreply.github.com>
2025-03-14 16:13:42 -0400

    Time: 211 ms (19.45%) | Memory: 23.4 MB (26.26%) - LeetSync

--- 817-design-hashmap/design-hashmap.py
+++ 817-design-hashmap/design-hashmap.py
@@ -1,42 +1,46 @@
 class Node:
-    def __init__(self, key, val):
+    def __init__(self, key, value):
         self.key = key
-        self.val = val
+        self.value = value
         self.next = None
+
 
 class MyHashMap(object):
 
     def __init__(self):
-        self.map = [Node(0,0) for i in range(10**4)]
+        self.storage = [Node(0,0) for i in range(10**4)] 
         
 
     def put(self, key, value):
-        index = key % len(self.map)
-        cur = self.map[index]
+        idx = key % len(self.storage)
+        cur = self.storage[idx]
+
         while cur.next:
             if cur.next.key == key:
-                cur.next.val = value
-                return
+                cur.next.value = value
+                return 
             cur = cur.next
         cur.next = Node(key, value)
         
 
     def get(self, key):
-        index = key % len(self.map)
-        cur = self.map[index]
+        idx = key % len(self.storage)
+        cur = self.storage[idx]
+
         while cur.next:
             if cur.next.key == key:
-                return cur.next.val
+                return cur.next.value 
             cur = cur.next
         return -1
         
 
     def remove(self, key):
-        index = key % len(self.map)
-        cur = self.map[index]
+        idx = key % len(self.storage)
+        cur = self.storage[idx]
+
         while cur.next:
             if cur.next.key == key:
-                cur.next = cur.next.next
+                cur.next = cur.next.next 
                 return
             cur = cur.next
         
