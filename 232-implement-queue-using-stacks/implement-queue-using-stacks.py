df20f251fb7b1774367a9f74cba0cb3ea650e452
keyur-patel-21 <100202156+keyur-patel-21@users.noreply.github.com>
2025-03-14 16:45:52 -0400

    Time: 0 ms (100.00%) | Memory: 12.5 MB (18.82%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.py
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.py
@@ -1,31 +1,34 @@
 class MyQueue(object):
 
     def __init__(self):
-        self.inStack = []
-        self.outStack = []
+        self.stack1 = []
+        self.stack2 = []
+        
 
     def push(self, x):
-        self.inStack.append(x)
+        self.stack1.append(x)
         
 
     def pop(self):
-        if not self.outStack:
-            while self.inStack:
-                self.outStack.append(self.inStack.pop())
-        return self.outStack.pop()
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2.pop()
         
 
     def peek(self):
-        if not self.outStack:
-            while self.inStack:
-                self.outStack.append(self.inStack.pop())
-        return self.outStack[-1]
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2[-1]
         
 
     def empty(self):
-        if len(self.inStack) == 0 and len(self.outStack) == 0:
+        if  (not self.stack1 and not self.stack2):
             return True
-        return False
+        else:
+            return False
+
         
 
 
