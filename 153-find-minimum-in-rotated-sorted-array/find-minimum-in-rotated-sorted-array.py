b04c337499a375e6c765453565e20a28f7789f01
keyur-patel-21 <100202156+keyur-patel-21@users.noreply.github.com>
2025-03-16 14:37:48 -0400

    Time: 0 ms (100.00%) | Memory: 12.6 MB (27.56%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -12,7 +12,7 @@ class Solution(object):
 
             mid = (l + r) // 2
 
-            if ((mid == 0 or nums[mid] <= nums[mid-1]) and (mid == len(nums)-1 or nums[mid] <= nums[mid+1]) ):
+            if ((mid >= 0 and nums[mid] <= nums[mid-1]) and (mid <= len(nums)-2 or nums[mid] <= nums[mid+1]) ):
                 return nums[mid]
             elif nums[mid] <= nums[r]:
                 r = mid - 1
