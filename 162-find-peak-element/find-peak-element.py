a1150f64da9c11f854905e5047128541fcd3dad2
keyur-patel-21 <100202156+keyur-patel-21@users.noreply.github.com>
2025-03-17 10:42:38 -0400

    Time: 0 ms (100.00%) | Memory: 12.5 MB (51.59%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -1,27 +1,35 @@
+# Time Complexity: O(log N) - We use a binary search approach, reducing the search space by half in each iteration.
+# Space Complexity: O(1) - We use a constant amount of extra space.
+# 
+# Approach:
+# - If the array has only one element, return its index (0).
+# - Check if the first or last element is a peak, return their index if true.
+# - Use binary search to find a peak element:
+#   - If the middle element is greater than its neighbors, return its index.
+#   - If the left neighbor is greater, move left (`r = m - 1`).
+#   - Otherwise, move right (`l = m + 1`).
+# - The algorithm ensures finding a peak in logarithmic time.
+
+
 class Solution(object):
     def findPeakElement(self, nums):
         if len(nums) == 1:
             return 0
+
         if nums[0] > nums[1]:
             return 0
         if nums[-1] > nums[-2]:
             return len(nums) - 1
 
-        l, r = 1, len(nums) - 2
+        l, r = 0, len(nums) - 1
 
         while l <= r:
-            mid = (l + r) // 2
+            m = (l + r) // 2
 
-            if nums[mid] > nums[mid - 1] and nums[mid] > nums[mid + 1]:
-                return mid
-            elif nums[mid] > nums[mid - 1]:
-                l = mid + 1
+            if nums[m] > nums[m-1] and nums[m] > nums[m+1]:
+                return m
+            elif nums[m-1] > nums[m]:
+                r = m - 1
             else:
-                r = mid - 1
-
-        
-                
-
-
-
+                l = m + 1
         
