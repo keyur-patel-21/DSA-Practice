5f58597bd8d283b42a44bd7bedd063af64712d9d
keyur-patel-21 <100202156+keyur-patel-21@users.noreply.github.com>
2025-03-23 16:38:12 -0400

    Time: 11 ms (50.10%) | Memory: 13.4 MB (84.72%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,28 +1,39 @@
 class Solution(object):
     def canFinish(self, numCourses, prerequisites):
-        preMap = {i:[] for i in range(numCourses)}
-        for crs, pre in prerequisites:
-            preMap[crs].append(pre)
-
-        Visitset = set()
-
-        def dfs(crs):
-            if crs in Visitset:
-                return False
-            if preMap[crs] == []:
-                return True
-            
-            Visitset.add(crs)
-
-            for pre in preMap[crs]:
-                if not dfs(pre): return False
-            
-            Visitset.remove(crs)
-            preMap[crs] = []
-            return True
-
-        for i in range(numCourses):
-            if not dfs(i): return False
-        return True
-
         
+        indArray = [0] * numCourses
+        adjList = {}
+
+        for pre in prerequisites:
+            indArray[pre[0]] += 1
+            if pre[1] in adjList:
+                adjList[pre[1]].append(pre[0])
+            else:
+                adjList[pre[1]] = [pre[0]]
+
+        q = collections.deque() 
+        count = 0
+
+        for i in range(len(indArray)):
+            if indArray[i] == 0:
+                q.append(i)
+                count += 1
+        
+        if count == numCourses:
+            return True
+        if not q:
+            return False
+
+        while q:
+            cur = q.popleft()
+            if cur in adjList:
+                dependents = adjList[cur]
+                for dep in dependents:
+                    indArray[dep] -= 1
+                    if indArray[dep] == 0:
+                        q.append(dep)
+                        count += 1
+                        if count == numCourses:
+                            return True
+        
+        return False
