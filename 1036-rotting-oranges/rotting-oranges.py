719da5b5aa1f42b2990a0c3040d8ef07d1c3b757
keyur-patel-21 <100202156+keyur-patel-21@users.noreply.github.com>
2025-03-24 11:42:39 -0400

    Time: 7 ms (56.19%) | Memory: 12.4 MB (53.23%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,9 +1,7 @@
 class Solution(object):
     def orangesRotting(self, grid):
         q = collections.deque()
-        time = 0
-        fresh = 0
-        dirs = [[0,1], [1,0], [0,-1], [-1,0]]         
+        time, fresh = 0, 0 
 
         for i in range(len(grid)):
             for j in range(len(grid[0])):
@@ -15,6 +13,7 @@ class Solution(object):
         if fresh == 0:
             return time
 
+        dirs = [[0,1], [1,0], [0,-1], [-1,0]]
         while q:
             level = len(q)
             for i in range(level):
@@ -22,16 +21,16 @@ class Solution(object):
                 for dire in dirs:
                     nr = cur[0] + dire[0]
                     nc = cur[1] + dire[1]
-                    if 0 <= nr <len(grid) and 0 <= nc <len(grid[0]):
+
+                    if 0<= nr <len(grid) and 0<= nc <len(grid[0]):
                         if grid[nr][nc] == 1:
                             grid[nr][nc] = 2
                             q.append((nr, nc))
                             fresh -= 1
-
             time += 1
 
-        
         if fresh != 0:
             return -1
 
-        return time -1 
+        return time - 1
+        
