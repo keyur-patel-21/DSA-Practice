38be46ab07da53b9ba3b88dc05065a022ba37143
keyur-patel-21 <100202156+keyur-patel-21@users.noreply.github.com>
2025-03-24 17:28:36 -0400

    Time: 1024 ms (11.34%) | Memory: 16.6 MB (36.41%) - LeetSync

--- 817-design-hashmap/design-hashmap.py
+++ 817-design-hashmap/design-hashmap.py
@@ -7,11 +7,13 @@ class Node:
 class MyHashMap(object):
 
     def __init__(self):
-        self.map = [Node(0,0) for _ in range(10**3)]
+        self.map = [Node(0,0) for i in range(10*3)]
+        
 
     def put(self, key, value):
         idx = key % len(self.map)
         cur = self.map[idx]
+
         while cur.next:
             if cur.next.key == key:
                 cur.next.value = value
@@ -22,20 +24,26 @@ class MyHashMap(object):
     def get(self, key):
         idx = key % len(self.map)
         cur = self.map[idx]
+
         while cur.next:
             if cur.next.key == key:
                 return cur.next.value
             cur = cur.next
-        return -1        
+
+        return -1
+        
 
     def remove(self, key):
         idx = key % len(self.map)
         cur = self.map[idx]
+
         while cur.next:
             if cur.next.key == key:
                 cur.next = cur.next.next
-                return         
+                return
             cur = cur.next
+        
+        
 
 
 # Your MyHashMap object will be instantiated and called as such:
