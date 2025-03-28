7b1afc991fbb749afd657c28364f786e888d35b1
keyur-patel-21 <100202156+keyur-patel-21@users.noreply.github.com>
2025-03-28 09:36:28 -0400

    Time: 246 ms (42.47%) | Memory: 27.6 MB (87.40%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,23 +1,30 @@
 class Solution(object):
     def numIslands(self, grid):
-        q = collections.deque()
-        count = 0
+        island = 0
+        m = len(grid)
+        n = len(grid[0])
+
         dirs = [[0,1], [1,0], [0,-1], [-1,0]]
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == '1':
-                    count += 1
-                    q.append((i, j))
-                    grid[i][j] = '0'
-                    while q:
-                        # for i in range(len(q)):
-                            cur = q.popleft()
-                            for dire in dirs:
-                                nr = cur[0] + dire[0]
-                                nc = cur[1] + dire[1]
-                                if 0<=nr<len(grid) and 0<=nc<len(grid[0]):
-                                    if grid[nr][nc] == '1':
-                                        grid[nr][nc] = '0'
-                                        q.append((nr, nc))
-        
-        return count
+
+        def dfs(grid, i ,j):
+            #base
+            if not(0<=i<m and 0<=j<n):
+                return
+            if grid[i][j] != "1":
+                return
+
+            # logic
+            grid[i][j] = "0"
+            for dire in dirs:
+                r = i + dire[0]
+                c = j + dire[1]
+                dfs(grid, r, c)
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "1":
+                    island += 1
+                    dfs(grid, i , j)
+                    
+
+        return island
