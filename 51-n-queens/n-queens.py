7637b97232a5567fa719c67760c5e6e9654495e0
keyur-patel-21 <100202156+keyur-patel-21@users.noreply.github.com>
2025-03-31 11:24:49 -0400

    Time: 51 ms (37.88%) | Memory: 12.5 MB (75.06%) - LeetSync

---
+++ 51-n-queens/n-queens.py
@@ -0,0 +1,61 @@
+class Solution(object):
+    def solveNQueens(self, n):
+        result = []
+        matrix = [[None for _ in range(n)] for _ in range(n)]
+
+        def helper(i):
+            #base
+            if i == n:
+                path = []
+                for r in matrix:
+                    rowString = ""
+                    for c in r:
+                        if c:
+                            rowString += "Q"
+                        else:
+                            rowString += "."
+                    path.append(rowString)
+                result.append(path)
+                return
+
+
+            # logic
+            for j in range(n):
+                if isSafe(i, j):
+                    matrix[i][j] = True
+                    helper(i+1)
+                    matrix[i][j] = None
+
+        def isSafe(i , j):
+            r = i
+            c = j
+
+            while r>=0:
+                if matrix[r][c]:
+                    return False
+                r -= 1
+
+            r, c = i, j                
+
+            while r>=0 and c>=0:
+                if matrix[r][c]:
+                    return False
+                r -= 1
+                c -= 1
+
+            r, c = i, j
+
+            while r>=0 and c<n:
+                if matrix[r][c]:
+                    return False
+                r -= 1
+                c += 1
+
+            r, c = i, j
+            
+            return True
+
+
+
+        helper(0)
+        return result
