66b0e103a34f522ec5e75945c7e7ad04cd4b9bf3
keyur-patel-21 <100202156+keyur-patel-21@users.noreply.github.com>
2025-04-01 11:30:46 -0400

    Time: 937 ms (23.72%) | Memory: 12.7 MB (88.66%) - LeetSync

--- 282-expression-add-operators/expression-add-operators.py
+++ 282-expression-add-operators/expression-add-operators.py
@@ -4,34 +4,28 @@ class Solution(object):
 
         def helper(pivot, calc, tail, path):
             # base
-            
             if pivot == len(num):
                 if calc == target:
                     result.append(path)
-                return
 
-
-            #logic
+            # logic
             for i in range(pivot, len(num)):
-
                 if num[pivot] == "0" and pivot != i:
-                    continue 
-
-                currNumStr = num[pivot:i+1]
-                currNum = int(currNumStr)
+                    return
+                    
+                currNum = int(num[pivot:i+1])
 
                 if pivot == 0:
-                    helper(i+1, currNum, currNum, path + currNumStr)
+                    helper(i+1, currNum, currNum, path + str(currNum))
                 else:
                     # +
-                    helper(i+1, calc+currNum, currNum, path + "+" +currNumStr)
+                    helper(i+1, calc + currNum, currNum, path + "+" + str(currNum))
 
                     # -
-                    helper(i+1, calc-currNum, -currNum, path + "-" + currNumStr)
+                    helper(i+1, calc - currNum, -currNum, path + "-" + str(currNum))
 
-                    # * 
-                    helper(i+1, (calc-tail)+(tail*currNum), tail*currNum, path + "*" + currNumStr)
-
+                    # *
+                    helper(i+1, (calc-tail) + (currNum*tail), currNum*tail, path + "*" + str(currNum))
         helper(0, 0, 0, "")
         return result
         
