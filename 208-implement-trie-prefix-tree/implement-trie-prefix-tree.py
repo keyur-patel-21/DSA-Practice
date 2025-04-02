a18f32afa63f8c2b8a5e3aee19d0da111c1dede7
keyur-patel-21 <100202156+keyur-patel-21@users.noreply.github.com>
2025-04-02 11:52:31 -0400

    Time: 82 ms (73.36%) | Memory: 39.1 MB (41.67%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
@@ -1,45 +1,32 @@
 class TrieNode:
     def __init__(self):
         self.isEnd = False
-        self.children = [None] * 26
+        self.children = {}  # Use a dictionary instead of a fixed list
 
-class Trie(object):
-
+class Trie:
     def __init__(self):
-        self.root = TrieNode()        
+        self.root = TrieNode()
 
     def insert(self, word):
         curr = self.root
         for c in word:
-            if curr.children[ord(c) - ord("a")] == None:
-                curr.children[ord(c) - ord("a")] = TrieNode()
-            curr = curr.children[ord(c) - ord("a")]
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
         curr.isEnd = True
-
-        
 
     def search(self, word):
         curr = self.root
         for c in word:
-            if curr.children[ord(c) - ord("a")] == None:
+            if c not in curr.children:
                 return False
-            curr = curr.children[ord(c) - ord("a")]
+            curr = curr.children[c]
         return curr.isEnd
-        
 
     def startsWith(self, prefix):
         curr = self.root
         for c in prefix:
-            if curr.children[ord(c) - ord("a")] == None:
+            if c not in curr.children:
                 return False
-            curr = curr.children[ord(c) - ord("a")]
+            curr = curr.children[c]
         return True
-
-        
-
-
-# Your Trie object will be instantiated and called as such:
-# obj = Trie()
-# obj.insert(word)
-# param_2 = obj.search(word)
-# param_3 = obj.startsWith(prefix)
