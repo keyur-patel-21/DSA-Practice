03a49b83f122274c65484fb73a2e9f542ed652c1
keyur-patel-21 <100202156+keyur-patel-21@users.noreply.github.com>
2025-04-07 15:09:36 -0400

    Time: 0 ms (100.00%) | Memory: 12.5 MB (59.98%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,6 +1,15 @@
 class Solution(object):
     def myPow(self, x, n):
+        if n == 0:
+            return 1
         
+        if n < 0:
+            x = 1.0 / x
+            n = -n
 
-        return x**n
-        
+        result = self.myPow(x, n // 2)
+
+        if n % 2 == 0:
+            return result * result
+        else:
+            return result * result * x
