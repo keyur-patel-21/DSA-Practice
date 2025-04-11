fc8abde67008df27ad35bfa81ed06768ccb76da9
keyur-patel-21 <100202156+keyur-patel-21@users.noreply.github.com>
2025-04-11 12:22:45 -0400

    Time: 39 ms (93.03%) | Memory: 13.9 MB (9.13%) - LeetSync

---
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def findAnagrams(self, s, p):
+        m = len(s)
+        n = len(p)
+        result = []
+
+        freqMap = {}
+        match = 0
+
+        for ch in p:
+            if ch in freqMap:
+                freqMap[ch] += 1
+            else:
+                freqMap[ch] = 1
+
+        for i in range(m):
+            # in 
+            if s[i] in freqMap:
+                freqMap[s[i]] -= 1
+                if freqMap[s[i]] == 0:
+                    match += 1
+
+            # out
+            if i >= n:
+                if s[i-n] in freqMap:
+                    freqMap[s[i-n]] += 1
+                    if freqMap[s[i-n]] == 1:
+                        match -= 1
+
+            if match == len(freqMap):
+                result.append(i-n+1)
+            
+        return result
+
+             
+        
