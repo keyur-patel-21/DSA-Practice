0ce3b613c7598e55fa4904edd720ee889da775f7
keyur-patel-21 <100202156+keyur-patel-21@users.noreply.github.com>
2025-04-14 16:54:49 -0400

    Time: 0 ms (100.00%) | Memory: 12.7 MB (27.94%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -1,32 +1,18 @@
-# Approach:
-# - This function finds the minimum element in a rotated sorted array.
-# - The idea is to use a modified binary search.
-# - If the array is already sorted (nums[l] <= nums[r]), return nums[l].
-# - Otherwise, find the middle element and check if it is the minimum.
-# - If nums[mid] is smaller than both its left and right neighbors, return nums[mid].
-# - If nums[mid] is in the sorted right half, move left (r = mid - 1).
-# - Otherwise, move right (l = mid + 1).
-#
-# Time Complexity (TC): O(log N) → Since we are using binary search.
-# Space Complexity (SC): O(1) → No extra space is used apart from variables.
-
 class Solution(object):
     def findMin(self, nums):
-        if len(nums) == None or len(nums) == 0:
-            return -1
+        l, r = 0, len(nums)-1
+        if nums == None or len(nums) == 0:
+            return None
 
-        l, r = 0, len(nums) - 1
-
-        while l <= r:
-            
+        while l<=r:
             if nums[l] <= nums[r]:
                 return nums[l]
+            m = (l+r) // 2
 
-            mid = (l + r) // 2
-
-            if ((mid >= 0 and nums[mid] <= nums[mid-1]) and (mid <= len(nums)-2 or nums[mid] <= nums[mid+1])):
-                return nums[mid]
-            elif nums[mid] <= nums[r]:
-                r = mid - 1
-            else:
-                l = mid + 1
+            if nums[m-1] > nums[m] < nums[m+1]:
+                return nums[m]
+            if nums[l] <= nums[m] and nums[m] > nums[r]:   #we are in left
+                
+                l = m + 1
+            else:   #we are in right
+                r = m - 1
