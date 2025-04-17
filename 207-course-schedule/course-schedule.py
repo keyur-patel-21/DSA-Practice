1207c5725251edfc1087841015ad5ea25670c138
keyur-patel-21 <100202156+keyur-patel-21@users.noreply.github.com>
2025-04-17 09:25:07 -0400

    Time: 9 ms (59.40%) | Memory: 16 MB (32.39%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -4,30 +4,29 @@ class Solution(object):
         visited = set()
 
         for course, requirement in prerequisites:
-            if course in preList:
-                preList[course].append(requirement)
-            else:
-                preList[course] = [requirement]
+            preList[course].append(requirement)
 
         def dfs(course):
             if course in visited:
                 return False
             
-            if len(preList[course]) == 0:
+            if not preList[course]:
                 return True
 
             visited.add(course)
 
-            for required_course in preList[course]:
-                if not dfs(required_course): return False
-            
-            visited.remove(course) #backTrack
-            preList[course] = []   #BackTrack
+            for crs in preList[course]:
+                if not dfs(crs):
+                    return False
+
+            visited.remove(course)
+            preList[course] = []
 
             return True
 
         for i in range(numCourses):
-            if not dfs(i): return False
+            if not dfs(i):
+                return False
         return True
 
-        
+                
