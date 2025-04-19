ec768f839c25abf47c4ddad0f26805d6a2094d9b
keyur-patel-21 <100202156+keyur-patel-21@users.noreply.github.com>
2025-04-19 10:03:23 -0400

    Time: 3 ms (94.98%) | Memory: 13.8 MB (83.39%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.py
+++ 740-delete-and-earn/delete-and-earn.py
@@ -1,19 +1,19 @@
 class Solution(object):
     def deleteAndEarn(self, nums):
-
+        
         if not nums:
-            return 0
+            return None
 
-        maximum = max(nums)
-        dp = [0] * (maximum+1) 
+        dp = [0] * (int(max(nums)) + 1)
 
         for num in nums:
             dp[num] += num
 
-        if maximum == 1:
-            return dp[1] 
-        
-        for i in range(2, maximum+1):
-            dp[i] = max(dp[i-1], dp[i] + dp [i-2])
+        if len(nums) == 1:
+            return nums[0]
 
-        return dp[maximum]
+        for i in range(2, int(max(nums)) + 1):
+            dp[i] = max(dp[i-1], (dp[i]+dp[i-2]))
+
+        return dp[-1]
+        
